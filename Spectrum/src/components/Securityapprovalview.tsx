861bf09477fe4a850a54dde9ce7ad08833017c4b
ishwar <sanketkhairnar390.com>
2025-12-16 17:53:54 +0530

    changes in css

--- Spectrum/src/components/Securityapprovalview.tsx
+++ Spectrum/src/components/Securityapprovalview.tsx
@@ -920,9 +920,7 @@ function Securityapprovalview() {
                     >
                       <td>{currentStartIndex + idx + 1}</td>
                       <td>{entry.visitorEntry_Gatepass}</td>
-                      <td>
-                        <strong>{entry.visitorEntry_visitorName}</strong>
-                      </td>
+                      <td>{entry.visitorEntry_visitorName}</td>
                       <td>{entry.visitorEntry_Vehicletype}</td>
                       <td>{entry.visitorEntry_Vehicleno}</td>
                       <td>{formatDateOnly(entry.visitorEntry_Date)}</td>
@@ -1130,9 +1128,7 @@ function Securityapprovalview() {
                     >
                       <td>{historyStartIndex + idx + 1}</td>
                       <td>{entry.visitorEntry_Gatepass}</td>
-                      <td>
-                        <strong>{entry.visitorEntry_visitorName}</strong>
-                      </td>
+                      <td>{entry.visitorEntry_visitorName}</td>
                       <td>{entry.visitorEntry_Vehicletype}</td>
                       <td>{entry.visitorEntry_Vehicleno}</td>
                       <td>{formatDateOnly(entry.visitorEntry_Date)}</td>
